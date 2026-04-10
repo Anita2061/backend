ac4fd8da239bd008f541f4a28b2a01972a6df8fe
Anita2061 <anitagyawali101@gmail.com>
2026-04-11 00:08:43 +0545

    Configure CORS and environment variables for deployment

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = [""]
 ALLOWED_HOSTS = ['https://backend-mg40.onrender.com']
 
 ROOT_URLCONF = 'config.urls'
@@ -116,11 +116,11 @@ MONGO_URI = os.getenv("MONGO_URI", "")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "project_db")
 
 # CORS (dev)
-CORS_ALLOW_ALL_ORIGINS = "https://frontend-theta-eight-79.vercel.app/",
+CORS_ALLOW_ALL_ORIGINS = "https://frontend-delta-sooty-87.vercel.app/products",
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ["https://frontend-theta-eight-79.vercel.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://frontend-delta-sooty-87.vercel.app/products"]
 
 # DRF defaults (keep simple for now)
 REST_FRAMEWORK = {
